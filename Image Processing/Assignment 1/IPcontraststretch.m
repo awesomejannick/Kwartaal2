74cb12167b2c502f2fdc6f400f4fd3f2efd477a1
jannick <jannickboonstra@live.nl>
2018-11-26 17:23:11 +0100

    fixed error in report

--- Image Processing/Assignment 1/IPcontraststretch.m
+++ Image Processing/Assignment 1/IPcontraststretch.m
@@ -1,26 +1,7 @@
-% Maximizes the contrast of Im through linear stretching
-function Im2 = IPcontraststretch(Im)
-    N = 8; %Number of bits
-    Im2 = Im;
-    
-    %Determine max and min intensities:
-    m = 2^N-1;
-    M = 0;
-    for i = 1:size(Im, 1)
-        for j = 1:size(Im, 2)
-            if Im(i, j) > M
-                M = Im(i, j);
-            elseif Im(i,j) < m
-                m = Im(i, j);
-            end
-        end
-    end
-    
-    %Perform linear contrast stretching:
-    c = 2.0^N / double(M-m);
-    for i = 1:size(Im, 1)
-        for j = 1:size(Im, 2)
-            Im2(i, j) = c * (Im(i, j) - m);
-        end
-    end
+% Maximizes the contrast of Im (N-byte image) through linear stretching
+function Im2 = IPcontraststretch(Im, N)
+    M = max(Im(:));
+    m = min(Im(:));
+    c = (2.0^N - 1) / double(M-m);
+    Im2 = c * (Im - m);
 end
--- Image Processing/Assignment 1/script1_2.m
+++ Image Processing/Assignment 1/script1_2.m
@@ -5,20 +5,21 @@ clear all;
 trui = imread('../images/trui.tif');
 
 % Stretch contrast of image
-trui_stretched = IPcontraststretch(trui);
-imwrite(trui_stretched, 'output/trui_stretched.tif');
+trui_stretched = IPcontraststretch(trui, 8);
+imwrite(trui, 'output/trui.png');
+imwrite(trui_stretched, 'output/trui_stretched.png');
 
 % Plot the input image
 figure;
 colormap(gray(256));
 imshow(trui);
-title('trui.tif')
+title('trui')
 
 % Plot the stretched image
 figure;
 colormap(gray(256));
 imshow(trui_stretched);
-title('trui\_stretched.tif')
+title('trui\_stretched')
 
 % Plot histograms of the original and the stretched image
 figure;hold on;
@@ -26,4 +27,4 @@ histogram(trui);
 histogram(trui_stretched);
 title('histograms of trui and trui\_stretched');
 legend('trui', 'trui\_stretched');
-saveas(gcf, 'output/histogram.tif');
+saveas(gcf, 'output/histogram.png');
