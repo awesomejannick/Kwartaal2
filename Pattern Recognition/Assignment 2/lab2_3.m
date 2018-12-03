d3de26c1302702dace46652e8008fe7d2f5eaf04
Rick de Jonge <dejonge00@live.nl>
2018-12-03 13:28:13 +0100

    pr 2-3

--- Pattern Recognition/Assignment 2/lab2_3.m
+++ Pattern Recognition/Assignment 2/lab2_3.m
@@ -3,6 +3,8 @@ covmat = [1 0; 0 2];
 disp(mu);
 disp(covmat);
 
+% ---- 1
+
 [x, y] = meshgrid(-10:0.1: 10);
 z = zeros(size(x, 1));
 for i = 1: size(x, 1)
@@ -15,5 +17,16 @@ figure;
 m = mesh(x, y, z);
 title('2D Gaussian Mesh');
 
-disp(z);
-disp(mahal(z, [10 10]));
+% ---- 2
+stddev = sqrt([covmat(1,1), covmat(2,2)]);
+disp(stddev);
+
+qs = [10 10; 0 0; 3 4; 6 8];
+
+for i = 1: size(qs, 1)
+    a = sqrt((qs(i,:) - mu)*inv(covmat)*(qs(i,:)-mu)');
+    disp([qs(i,:), a]);
+end
+
+x = mvnrnd(mu,covmat,100000);
+disp(sqrt(mahal(qs,x)));
