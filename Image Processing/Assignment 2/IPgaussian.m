6a2996160c58dd8431f0e635e8581c2919f2f7e2
Rick <dejonge00@live.nl>
2018-12-06 11:09:49 +0100

    IPgaussian

--- Image Processing/Assignment 2/IPgaussian.m
+++ Image Processing/Assignment 2/IPgaussian.m
@@ -1,3 +1,10 @@
 function H = IPgaussian(D0, M, N)
-    
+    H = zeros(M, N);
+    middle = [(M+1)/2, (N+1)/2];
+    for i = 1: M
+        for j = 1: N
+            d = max(abs(i - middle(1)), abs(j - middle(2)));
+            H(i, j) = exp((-d) / (2 * D0));
+        end
+    end
 end
