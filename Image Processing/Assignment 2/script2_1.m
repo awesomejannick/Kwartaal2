e5fa991c1bf56b899709876e387c0b2b0a048455
awesomejannick <jannickboonstra@live.nl>
2018-12-10 13:37:49 +0100

    The moon is blurry

--- Image Processing/Assignment 2/script2_1.m
+++ Image Processing/Assignment 2/script2_1.m
@@ -3,18 +3,17 @@ close all;
 clear all;
 
 %Load input image
-trui = imread('../images/trui.tif');
+moon = imread('../images/blurrymoon.tif');
 
 %Show image and its gradient
 figure;
 colormap(gray(256));
 subplot(1,3,1);
-imshow(trui);
-title('trui');
+imshow(moon);
+title('Blurry moon');
 subplot(1,3,2);
-imshow(IPgradient(trui, 1));
+imshow(IPgradient(moon, 1));
 title('Prewitt Gradient');
 subplot(1,3,3);
-imshow(IPgradient(trui, 2));
-disp(IPgradient(trui, 2))
+imshow(IPgradient(moon, 2));
 title('Sobel Gradient');
