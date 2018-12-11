54e92d61c7bf7db899500112334d61d84dc25fd9
awesomejannick <jannickboonstra@live.nl>
2018-12-11 14:58:17 +0100

    lab3_3 finish

--- Pattern Recognition/Assignment 3/lab3_3.m
+++ Pattern Recognition/Assignment 3/lab3_3.m
@@ -4,13 +4,24 @@ k3 = load('lab3_3_cat3.mat'); k3 = k3.x_w3;
 u = [0.5, 1.0, 0.0];
 v = [0.31, 1.51, -0.5];
 w = [-1.7, -1.7, -1.7];
-h = 1;
-disp(GaussianSphere(u, k1, 1));
-disp(GaussianSphere(u, k2, 1));
-disp(GaussianSphere(u, k3, 1));
-disp(GaussianSphere(v, k1, 1));
-disp(GaussianSphere(v, k2, 1));
-disp(GaussianSphere(v, k3, 1));
-disp(GaussianSphere(w, k1, 1));
-disp(GaussianSphere(w, k2, 1));
-disp(GaussianSphere(w, k3, 1));
+for i = 1:2
+    u1 = GaussianSphere(u, k1, i);
+    u2 = GaussianSphere(u, k2, i);
+    u3 = GaussianSphere(u, k3, i);
+    v1 = GaussianSphere(v, k1, i);
+    v2 = GaussianSphere(v, k2, i);
+    v3 = GaussianSphere(v, k3, i);
+    w1 = GaussianSphere(w, k1, i);
+    w2 = GaussianSphere(w, k2, i);
+    w3 = GaussianSphere(w, k3, i);
+
+    disp([u1/(u1+u2+u3) u2/(u1+u2+u3) u3/(u1+u2+u3)])
+    disp([v1/(v1+v2+v3) v2/(v1+v2+v3) v3/(v1+v2+v3)])
+    disp([w1/(w1+w2+w3) w2/(w1+w2+w3) w3/(w1+w2+w3)])
+end
+
+data = [k1;k2;k3];
+labels = [ones(10,1),ones(10,1)+1,ones(10,1)+2];
+for k = [1,5]
+    disp([KNN(u,k,data,labels) KNN(v,k,data,labels) KNN(w,k,data,labels)]);
+end
