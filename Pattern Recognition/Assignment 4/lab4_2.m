8e12b7d6217bfc0a2703da887b9dbf20b4200e43
jannick <jannickboonstra@live.nl>
2018-12-17 11:22:15 +0100

    myhough

--- Pattern Recognition/Assignment 4/lab4_2.m
+++ Pattern Recognition/Assignment 4/lab4_2.m
@@ -1,7 +1,12 @@
+%Load input and get its edges
 img = imread('Cameraman.tiff');
 edges = edge(img);
+
+%Perform hough transform with 2 implementations
 H1 = hough(edges);
 H2 = myhough(edges);
+
+%Plot results
 figure;
 subplot(1,2,1);
 imagesc(H1);
--- Pattern Recognition/Assignment 4/myhough.m
+++ Pattern Recognition/Assignment 4/myhough.m
@@ -1,13 +1,19 @@
+%Performs a hough transform on an edgemap
 function [H, theta, rho] = myhough(edges)
+    %Find all points belonging to an edge
     points = (find(edges==1))';
     r = zeros(length(points), 180);
+    
+    %Determine the angles used
     theta = -90:89;
     
+    %Calculate distances
     for i = 1:length(points)
         point = [mod(points(i),size(edges,1)) floor(points(i)/size(edges,1))];
         r(i,:) = round(point(2)*cosd(theta) + point(1)*sind(theta));
     end
     
+    %Create hough matrix
     rho = min(r,[],'all'):max(r,[],'all');
     r = r - min(r,[],'all') + 1;
     m = max(r,[],'all');
