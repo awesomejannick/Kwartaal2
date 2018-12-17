3348dce559ebebe9c691082f9f4878c2eff7917b
Rick de Jonge <dejonge00@live.nl>
2018-12-17 12:01:10 +0100

    4.4 code and fig

--- Pattern Recognition/Assignment 4/lab4_4.m
+++ Pattern Recognition/Assignment 4/lab4_4.m
@@ -1,14 +1,36 @@
 close all;
 clear all;
+figure;
+image = imread('HeadTool0002.bmp');
 
 % Q1
-image = double(imread('HeadTool0002.bmp'));
+imaged = double(image);
+
 % Q2
-image = image;
+imagec = adapthisteq(image);
+subplot(2, 2, 1);
+imshow(image);
+title('Original HeadTool0002.bmp');
+subplot(2, 2, 2);
+imshow(imagec);
+title('Contrast-limited Adaptive Histogram Equalization on HeadTool0002.bmp');
+
 % Q3
-[centers, radii] = imfindcircles(image, [20 40], 'Sensitivity', 0.99);
-disp(centers(1:6, :));
+[centers, radii] = imfindcircles(imagec, [20 40], 'Sensitivity', 0.9);
+disp(centers);
 
 colormap('gray');
-imagesc(image);
-viscircles(centers(1:6, :), radii(1:6, :), 'EdgeColor','b');
+subplot(2, 2, 3);
+imshow(imagec);
+title('Sensitivity: 0.9, all circles');
+viscircles(centers, radii, 'EdgeColor','b');
+
+% Q4
+subplot(2, 2, 4);
+imshow(imagec);
+title('Sensitivity: 0.9, best 2 circles');
+amount = 2;
+viscircles(centers(1:amount, :), radii(1:amount, :), 'EdgeColor','b');
+
+
+
